1f913fd3910ed3b3aa0245aeebf76abc1169418f
Liam Myles <liammyles@sbcglobal.net>
2019-02-16 22:01:01 +0000

    Finished files for 07

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -7,7 +7,7 @@ const theme = {
   red: "#FF0000",
   black: "#393939",
   grey: "#3A3A3A",
-  lightGrey: "#E1E1E1",
+  lightgrey: "#E1E1E1",
   offWhite: "#EDEDED",
   maxWidth: "1000px",
   bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
@@ -21,31 +21,34 @@ const StyledPage = styled.div`
 const Inner = styled.div`
   max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
-  padding: 20px;
+  padding: 2rem;
 `;
 
 injectGlobal`
-   @font-face {
+  @font-face {
     font-family: 'radnika_next';
     src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
     font-weight: normal;
     font-style: normal;
   }
-
-  html{
+  html {
     box-sizing: border-box;
     font-size: 10px;
   }
-    *, *:before, *:after{
-      box-sizing: inherit;
+  *, *:before, *:after {
+    box-sizing: inherit;
   }
-  body{
+  body {
     padding: 0;
     margin: 0;
-    font-size: 15px;
+    font-size: 1.5rem;
     line-height: 2;
+    font-family: 'radnika_next';
   }
-  
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
 `;
 
 class Page extends Component {
@@ -55,7 +58,6 @@ class Page extends Component {
         <StyledPage>
           <Meta />
           <Header />
-          <h1>Hello I'm the page component</h1>
           <Inner>{this.props.children}</Inner>
         </StyledPage>
       </ThemeProvider>
